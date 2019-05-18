bb8d28ffee740ce2d2c78c259a1b4d2cdc713494
Ronaldo <kikogamer@gmail.com>
2019-05-18 13:02:46 -0300

    tweet request test (post, destroy, update)

--- spec/requests/api/v1/tweets_spec.rb
+++ spec/requests/api/v1/tweets_spec.rb
@@ -93,4 +93,141 @@ RSpec.describe "Api::V1::Tweets", type: :request do
     end
   end
 
+  describe 'POST /api/v1/tweets' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :put, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      let(:user) { create(:user) }
+
+      context 'Valid params' do
+        context 'regular tweet' do
+          let(:tweet_params) { attributes_for(:tweet) }
+
+          it 'return created' do
+            post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            expect(response).to have_http_status(:created)
+          end
+
+          it 'returns right tweet in json' do
+            post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            expect(json).to include_json(tweet_params)
+          end
+
+          it 'create tweet' do
+            expect do
+              post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            end.to change { Tweet.count }.by(1)
+          end
+        end
+
+        context 'retweet' do
+          before { @tweet_original = create(:tweet) }
+          let(:tweet_params) { attributes_for(:tweet, tweet_original_id: @tweet_original.id) }
+
+          it 'return created' do
+            post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            expect(response).to have_http_status(:created)
+          end
+
+          it 'returns tweet in json' do
+            post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            expect(json).to include_json(tweet_params)
+          end
+
+          it 'returns original tweet in json' do
+            post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            expect(json['tweet_original']).to eql(serialized(Api::V1::TweetSerializer, @tweet_original))
+          end
+
+          it 'create tweet' do
+            expect do
+              post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+            end.to change { Tweet.count }.by(1)
+          end
+        end
+      end
+
+      context 'Invalid params' do
+        let(:tweet_params) { {foo: :bar} }
+
+        before { post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user) }
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/tweets/:tweet_id' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :put, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      context 'Resource owner' do
+        let(:user) { create(:user) }
+        before { @tweet = create(:tweet, user: user) }
+
+        it do
+          delete "/api/v1/tweets/#{@tweet.id}", headers: header_with_authentication(user)
+          expect(response).to have_http_status(:no_content)
+        end
+
+        it 'delete tweet' do
+          expect do
+            delete "/api/v1/tweets/#{@tweet.id}", headers: header_with_authentication(user)
+          end.to change { Tweet.count }.by(-1)
+        end
+      end
+
+      context 'Not resource owner' do
+        let(:user) { create(:user) }
+        let(:other_user) { create(:user) }
+        let(:tweet) { create(:tweet, user: other_user) }
+
+        before do
+          delete "/api/v1/tweets/#{tweet.id}", headers: header_with_authentication(user)
+        end
+
+        it { expect(response).to have_http_status(:forbidden) }
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/tweets/:tweet_id' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :put, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      context 'Resource owner' do
+        let(:user) { create(:user) }
+        let(:tweet) { create(:tweet, user: user) }
+        let(:tweet_params) { attributes_for(:tweet) }
+
+        before { put "/api/v1/tweets/#{tweet.id}", params: { tweet: tweet_params }, headers: header_with_authentication(user) }
+
+        it { expect(response).to have_http_status(:success) }
+
+        it 'returns tweet updated in json' do
+          post '/api/v1/tweets/', params: { tweet: tweet_params }, headers: header_with_authentication(user)
+          expect(json).to include_json(tweet_params)
+        end
+      end
+
+      context 'Not resource owner' do
+        let(:user) { create(:user) }
+        let(:other_user) { create(:user) }
+        let(:tweet) { create(:tweet, user: other_user) }
+        let(:tweet_params) { attributes_for(:tweet) }
+
+        before do
+          put "/api/v1/tweets/#{tweet.id}", params: { tweet: tweet_params }, headers: header_with_authentication(user)
+        end
+
+        it { expect(response).to have_http_status(:forbidden) }
+      end
+    end
+  end
 end
