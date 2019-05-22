edb6f7d133a693831535b6096f10f5a376e6b0d9
Ronaldo <kikogamer@gmail.com>
2019-05-21 22:13:57 -0300

    search with elasticsearch

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,15 @@ class User < ApplicationRecord #>
   acts_as_followable
   acts_as_follower
   has_many :tweets, dependent: :destroy
+  searchkick
+  
+  def search_data
+    { name: name, email: email }
+  end
 
   def timeline
-    timeline = tweets.map { |tweet| tweet }
-    all_following.each do |user|
+    timeline = self.tweets.map { |tweet| tweet }
+    self.all_following.each do |user|
       timeline += user.tweets.map { |tweet| tweet }
     end
     timeline.sort_by!(&:created_at).reverse
