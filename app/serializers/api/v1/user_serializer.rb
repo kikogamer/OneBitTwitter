c9881089bfff919771c31e5747d6631d88107812
Ronaldo <kikogamer@gmail.com>
2019-08-12 23:55:12 -0300

    following feature

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,10 +1,14 @@
 module Api
   module V1
     class UserSerializer < ActiveModel::Serializer #>
-      attributes :id, :name, :description, :email, :tweets_count, :followers_count, :following_count, :photo, :followed
+      attributes :id, :name, :description, :email, :tweets_count, :followers_count, :following_count, :photo, :followed, :followers
 
       def tweets_count
         object.tweets.count
+      end
+
+      def followers
+        object.followers_by_type('User')
       end
 
       def followers_count
